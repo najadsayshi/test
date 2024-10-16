762487beb9d36b14cd52add6d50b49f1e270e13c
najadsayshi <mrnajadas@gmail.com>
2024-10-16 06:10:07 +0530

    Two sum

---
+++ program1/twosum.py
@@ -0,0 +1,9 @@
+nums=[2,7,11,15]
+target=9
+
+prevEle={}
+for index,value in enumerate(nums):
+    diff=target-value
+    if diff in prevEle:
+        print([index,prevEle[diff]])
+    prevEle[value]=index
