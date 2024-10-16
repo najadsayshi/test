6da9d0d683bd16a5c392d8ff3a58f49d0643a157
najadsayshi <mrnajadas@gmail.com>
2024-10-16 06:03:46 +0530

    secondLargestInArray

--- test.py
+++ test.py
@@ -1,3 +1,12 @@
-print("hello world")
-print("ok")
-print("somethging wrong?")
+a=[1,2,3,4,5,6,7]
+
+largest=a[0]
+secondlargest=-1
+
+for i in range(1,len(a),1):
+    if a[i]>largest:
+        secondlargest=largest
+        largest=a[i]
+    elif a[i]<largest and a[i]>secondlargest:
+        secondlargest=a[i]
+print(secondlargest)
